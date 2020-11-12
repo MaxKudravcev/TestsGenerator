40c7514445dbaeb5078129f2dc9ac50e8eb51bda
MaxKudravcev <maxd4567@gmail.com>
2020-11-12 17:19:16 +0300

    Finished TestsGenerator.Lib

---
+++ TestsGeneratorLib/TestUnit.cs
@@ -0,0 +1,14 @@
+﻿namespace TestGenerator.Lib
+{
+    public class TestUnit
+    {
+        public string Test { get; }
+        public string Name { get; }
+
+        public TestUnit(string test, string name)
+        {
+            Test = test;
+            Name = name;
+        }
+    }
+}
